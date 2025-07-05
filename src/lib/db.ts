801fe15a89fb171a92f217988c5fccd48b933c52
diego <hermani457@gmail.com>
2025-07-04 21:03:36 -0400

    refactor: Ensure DB port is parsed as an integer or set to undefined

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 const connectionConfig = {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
